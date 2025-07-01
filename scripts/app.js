fed82edfe587d683fd8077b3d735f6cc06e2f7f7
shaunte99 <ksmatsetela99@gmail.com>
2025-07-01 18:01:28 +0200

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -12,7 +12,7 @@ fetch("data/daily.json")
 function showClickMessage() {
   const response = document.getElementById("click-response");
   response.classList.remove("hidden");
-  response.textContent = "You clicked it! That made my day. I hope this site makes you smile like you made me smile today 🦋";
+  response.textContent = "YOU CLICKED IT LESSSGOOOOO🦋";
 }
 
 function showMoodMessage(mood) {
